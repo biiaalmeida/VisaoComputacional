05422493dfaffb95ccb0c420a460315e0a0f3d75
Ana Beatriz Almeida da Silva <abeatrizpb@gmail.com>
2026-04-10 16:26:03 -0300

    Refactor quadrant limit calculations for dynamic grid sizing in astar_teste.c

--- astar_teste.c
+++ astar_teste.c
@@ -10,40 +10,71 @@
 #include "astar.c"
 
 /*
-   Quadrantes:
-   Q1 = linhas 0-4, colunas 0-4
-   Q2 = linhas 0-4, colunas 5-9
-   Q3 = linhas 5-9, colunas 0-4
-   Q4 = linhas 5-9, colunas 5-9
+   Os quadrantes são calculados dinamicamente a partir de ROW e COL.
+   Isso garante que quando a matriz crescer, o teste continua funcionando sem ajustes manuais.
 */
+
+void obterLimitesQuadrante(int quadrante, int *linMin, int *linMax, int *colMin, int *colMax) {
+    int metadeLinhas = ROW / 2;
+    int metadeColunas = COL / 2;
+
+    int linhaInicialSuperior = 0;
+    int linhaFinalSuperior = metadeLinhas - 1;
+    int linhaInicialInferior = metadeLinhas;
+    int linhaFinalInferior = ROW - 1;
+
+    int colunaInicialEsquerda = 0;
+    int colunaFinalEsquerda = metadeColunas - 1;
+    int colunaInicialDireita = metadeColunas;
+    int colunaFinalDireita = COL - 1;
+
+    switch (quadrante) {
+        case 1:
+            *linMin = linhaInicialSuperior;
+            *linMax = linhaFinalSuperior;
+            *colMin = colunaInicialEsquerda;
+            *colMax = colunaFinalEsquerda;
+            break;
+
+        case 2:
+            *linMin = linhaInicialSuperior;
+            *linMax = linhaFinalSuperior;
+            *colMin = colunaInicialDireita;
+            *colMax = colunaFinalDireita;
+            break;
+
+        case 3:
+            *linMin = linhaInicialInferior;
+            *linMax = linhaFinalInferior;
+            *colMin = colunaInicialEsquerda;
+            *colMax = colunaFinalEsquerda;
+            break;
+
+        case 4:
+            *linMin = linhaInicialInferior;
+            *linMax = linhaFinalInferior;
+            *colMin = colunaInicialDireita;
+            *colMax = colunaFinalDireita;
+            break;
+
+        default:
+            *linMin = linhaInicialSuperior;
+            *linMax = linhaFinalSuperior;
+            *colMin = colunaInicialEsquerda;
+            *colMax = colunaFinalEsquerda;
+    }
+}
 
 void gerarPontoQuadrante(int quadrante, int *linha, int *coluna) {
     int linMin, linMax, colMin, colMax;
 
-    switch (quadrante) {
-        case 1:
-            linMin = 0; linMax = 4;
-            colMin = 0; colMax = 4;
-            break;
+    obterLimitesQuadrante(quadrante, &linMin, &linMax, &colMin, &colMax);
 
-        case 2:
-            linMin = 0; linMax = 4;
-            colMin = 5; colMax = 9;
-            break;
-
-        case 3:
-            linMin = 5; linMax = 9;
-            colMin = 0; colMax = 4;
-            break;
-
-        case 4:
-            linMin = 5; linMax = 9;
-            colMin = 5; colMax = 9;
-            break;
-
-        default:
-            linMin = 0; linMax = 4;
-            colMin = 0; colMax = 4;
+    if (linMin > linMax || colMin > colMax) {
+        linMin = 0;
+        linMax = ROW - 1;
+        colMin = 0;
+        colMax = COL - 1;
     }
 
     do {
